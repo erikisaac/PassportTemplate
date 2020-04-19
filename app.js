648e3b482b017bdb49c2aecb2c55b6ebab125229
erikisaac <erik@erikisaac.com>
2020-04-19 07:47:20 -0600

    First login code.

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+app.use('/login');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/login.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
+
+module.exports = router;
