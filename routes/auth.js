e9b476ed48673b56d1ccd845b14e8928a262f015
erikisaac <erik@erikisaac.com>
2020-04-19 10:15:52 -0600

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+var passport = require('passport')
+  , OAuthStrategy = require('passport-oauth').OAuthStrategy;
+  
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
