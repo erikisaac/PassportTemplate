ad8e75fcdbe2a617bd8e7718a10a7ccdba75ced4
erikisaac <erik@erikisaac.com>
2020-04-19 12:50:07 -0600

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -7,11 +7,8 @@ var passport = require('passport')
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
 // });
-
-router.post('/login',
-	console.log("Passport POST request starting.")	
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
+console.log("Passport POST request starting.");	
+router.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login',
                                    failureFlash: true })
 );
 
