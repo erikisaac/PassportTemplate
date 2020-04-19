b4da378f3d30060870debdbaa8f5d5901ca2f5f8
erikisaac <erik@erikisaac.com>
2020-04-19 10:00:30 -0600

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
