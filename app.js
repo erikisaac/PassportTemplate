03ce467e25819c290ad1b33d1e79cee74bbda99a
erikisaac <erik@erikisaac.com>
2020-04-19 08:50:10 -0600

    code

--- app.js
+++ app.js
@@ -20,6 +20,21 @@ var app = express();
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,20 @@ var passport = require('passport')
 //   res.send('respond with a resource');
 // });
 
-
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function(err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// ));
 
 router.post('/login',
   passport.authenticate('local', { successRedirect: '/',
