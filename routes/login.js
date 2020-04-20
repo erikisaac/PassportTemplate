04926a49c187769450d0efe47f02da7c5758fd2b
erikisaac <erik@erikisaac.com>
2020-04-20 10:40:00 -0600

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
 
-// router.get('/login', function(req, res, next) {
-// 	res.render('/public/index.html');
-// });
+router.get('/login', function(req, res, next) {
+	res.render('/public/index.html');
+});
 
 router.post('/login', passport.authenticate('local', { successRedirect: '/', successFlash: true, successFlash: 'Erik Note: Success flash!', failureRedirect: '/', failureFlash: 'Erik Note: Failure flash!', failureFlash: true })
 );
