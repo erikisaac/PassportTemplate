bb5e13d05f42abea099b59351947fc6500ad51bd
erikisaac <erik@erikisaac.com>
2020-04-20 10:20:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ passport.deserializeUser(function(id, done) {
 });
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
