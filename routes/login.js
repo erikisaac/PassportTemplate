87f20401ac1b7dd23b1e06eca71a222d274de4aa
erikisaac <erik@erikisaac.com>
2020-04-20 12:40:21 -0600

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -5,15 +5,17 @@ var passport = require('passport')
 var mongodb = require("mongodb");
 var flash = require('connect-flash');
 
-var app = express();
-app.use(flash());
+// var app = express();
+// app.use(flash());
 
 // router.get('/', function(req, res, next) {
 // 	res.render('/public/index.html');
 // });
 
 
-router.post('/', passport.authenticate('local', { successRedirect: '/', successFlash: true, successFlash: 'Erik Note: Success flash!', failureRedirect: '/', failureFlash: 'Erik Note: Failure flash!', failureFlash: true }));
+router.post('/', passport.authenticate('local', { successRedirect: '/',
+	failureRedirect: '/')
+);
 
 mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb:Yellow1972!//heroku_3zgk6kbj:@ds155674.mlab.com:55674/heroku_3zgk6kbj", function (err, client) {
     if (err) {
@@ -26,16 +28,16 @@ myMongoDB = client.db("heroku_3zgk6kbj");
 passport.use(new LocalStrategy({
   passReqToCallback : true
 }, 
-function(req, username, password, done) {
+function(username, password, done) {
     myMongoDB.collection('users').findOne({ 'username': username }, function(err, user) {
       if (err) { return done(err); }
       if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
+        return done(null, false);
       }
       if (user.password != password) {
-        return done(null, false, { message: 'Incorrect password.' });
+        return done(null, false);
       }
-      return done(null, user, { message: 'Yay!' });
+      return done(null, user);
     });
   }
 ));
