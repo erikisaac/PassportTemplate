66ea5bf12cfa9124444516096ffd825810360b43
erikisaac <erik@erikisaac.com>
2020-04-21 12:02:20 -0600

    code

--- app.js
+++ app.js
@@ -34,11 +34,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/public')));
+// app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
 
 // app.use(express.static(path.join(__dirname, '/usersonly')));
 
-// app.use('/', indexRouter);
+
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 app.use('/login', loginRouter);
--- routes/usersonly.js
+++ routes/usersonly.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('usersonly/index2.html');
+  res.sendFile('index2.html');
 });
 
 module.exports = router;
