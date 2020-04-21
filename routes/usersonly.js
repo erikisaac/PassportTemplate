7b803380e053db9a583333b5de9d83fe699f168d
erikisaac <erik@erikisaac.com>
2020-04-21 15:12:21 -0600

    Update usersonly.js

--- routes/usersonly.js
+++ routes/usersonly.js
@@ -2,30 +2,18 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-// function authenticateUser(req) {
-// 	if (req.isAuthenticated()) {
-// 		router.use(express.static('usersonly'));
-// 	};
-// };
-
-//  GET home page. 
-// router.get('/index2.html', function(req, res, next) {
-// 	authenticateUser(req);
-// 	// res.sendFile('');
-// });
-
-router.get('/', checkAuthentication, function(req,res){
-    router.use(express.static('usersonly'));
-    res.sendFile((path.join(__dirname, '../usersonly/index2.html')));
-});
-
 function checkAuthentication(req,res,next){
 	if(req.isAuthenticated()){
-		res.redirect("/");	   
+
         next();
     } else {
         res.redirect("/");
     }
 };
+
+router.get('/', checkAuthentication, function(req,res){
+    router.use(express.static('usersonly'));
+    res.sendFile((path.join(__dirname, '../usersonly/index2.html')));
+});
 
 module.exports = router;
