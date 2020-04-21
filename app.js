c25a6ab1861e331967a6a56e039591b4b428c2f9
erikisaac <erik@erikisaac.com>
2020-04-21 08:42:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/usersonly', function(req, res, next) {
-	res.use(express.static(path.join(__dirname, '/usersonly')));
+	express.static(path.join(__dirname, '/usersonly'));
 	console.log("Erik Note: Users Only app.js route working.");
 });
 
