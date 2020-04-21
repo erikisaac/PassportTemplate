a1c8bfd439021b2edf01899ebe12ffc64fa78c44
erikisaac <erik@erikisaac.com>
2020-04-21 12:07:15 -0600

    code

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 var loginRouter = require('./routes/login');
-var usersonlyRouter = require('./routes/usersonly');
+// var usersonlyRouter = require('./routes/usersonly');
 
 var app = express();
 
@@ -44,7 +44,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 app.use('/login', loginRouter);
-app.use('/usersonly', usersonlyRouter);
+// app.use('/usersonly', usersonlyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.sendFile('index.html');
 });
 
+router.get('/index2', function(req, res, next) {
+  res.sendFile('/usersonly/index2.html');
+});
+
 module.exports = router;
