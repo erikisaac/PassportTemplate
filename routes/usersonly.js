0a23e2c33df9f590281fb4ebc565fbe48664ce3a
erikisaac <erik@erikisaac.com>
2020-04-21 12:37:17 -0600

    Update usersonly.js

--- routes/usersonly.js
+++ routes/usersonly.js
@@ -5,7 +5,7 @@ router.use(express.static('usersonly'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('index2.html');
+  res.sendFile('index2.html', { root: __dirname });
 });
 
 module.exports = router;
